d76f2241e0176ea697a9d6fc93ccec2076da19ee
bekaarcoder <s_shashank@outlook.com>
2025-01-20 21:20:49 +0530

    setting up root layout, shadcn and themes

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+const HomePage = () => {
+    return (
+        <div>
+            <Button>Submit</Button>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Nextstore';
+export const APP_DESCRIPTION =
+    process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+    'An ecommerce platform using Next.js';
+export const SERVER_URL =
+    process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
