bfe2a1a8f03b68d00c8dae2e6ebcd69738c1e31d
bekaarcoder <s_shashank@outlook.com>
2025-01-21 23:05:53 +0530

    prisma setup with product fetching and details page

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -2,9 +2,10 @@ import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import Image from 'next/image';
 import Link from 'next/link';
 import ProductPrice from './product-price';
+import { Product } from '@/types';
 
 interface Props {
-    product: any;
+    product: Product;
 }
 
 const ProductCard = ({ product }: Props) => {
--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,7 +1,8 @@
+import { Product } from '@/types';
 import ProductCard from './product-card';
 
 interface Props {
-    data: any;
+    data: Product[];
     title?: string;
     limit?: number;
 }
@@ -14,7 +15,7 @@ const ProductList = ({ data, title, limit }: Props) => {
             <h2 className="h2-bold mb-4">{title}</h2>
             {data.length > 0 ? (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {limitedData.map((product: any) => (
+                    {limitedData.map((product: Product) => (
                         <ProductCard key={product.slug} product={product} />
                     ))}
                 </div>
